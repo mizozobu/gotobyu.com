2e5661b6622972c45950e5c5b3823b07344c463a
mizozobu <hamc.m.so.j12@gmail.com>
2021-07-13 09:20:39 +0900

    feat: update learn-english page

--- src/components/pages/LearnEnglishPage/LearnEnglishPage.tsx
+++ src/components/pages/LearnEnglishPage/LearnEnglishPage.tsx
@@ -25,11 +25,25 @@ export const LearnEnglishPage: FC<Props> = () => (
               <Mark>
                 優等生でもTOEFLなどのテストで入学基準を満たすのは無理でしょう
               </Mark>
-              。
-              高校までの単語力は文法が理解できていることを前提に、入学基準を満たすためにより実用的な英語の勉強が必要です。
+              。 実際の大学生活を送るのに必要なレベルが求められます。
             </p>
 
+            <List>
+              <Item>英語で教科書が読めること</Item>
+              <Item>英語で論文を書けること</Item>
+              <Item>英語でプレゼンテーションができること</Item>
+              <Item>英語の授業を理解できること</Item>
+            </List>
+          </div>
+        </Pane>
+      </Section>
+
+      <Section id='英語の勉強方法' className='clear-both'>
+        <SectionTitle id='英語の勉強方法'>英語の勉強方法</SectionTitle>
+        <Pane>
+          <div className='space-y-4'>
             <p>
+              入学基準を満たすためには、高校までの単語力は文法が理解できていることを前提により実用的な英語の勉強が必要です。
               TOEFLなどのテストでは以下のの4つのカテゴリーで英語力が測られます。
             </p>
 
@@ -41,9 +55,10 @@ export const LearnEnglishPage: FC<Props> = () => (
             </List>
 
             <p>
-              SpeakingとListeningは慣れなので、アメリカの英語学校に行くなどで英語の環境に3~6ヶ月もいれば誰でも慣れてスコアも上がります。
-              しかし、ReadingとWritingは英語の環境にいるだけでは伸びないので、真面目に勉強したかどうかで伸び幅に差が生まれます。
-              英語学校に入学しつつ、真面目に勉強するというのが一般的な方法です。
+              どのカテゴリーについても<Mark>慣れる</Mark>ことが重要です。
+              SpeakingとListeningは、ホームステイや英語学校に通うことで3~6ヶ月も英語の環境に身を置けばある程度は慣れるでしょう。
+              しかし、日常生活で読み書きをする機会は多くはないので、ReadingとWritingは英語の環境で生活するだけでは伸びにくいことがあります。
+              ReadingとWritingを伸ばすには日記を書いたり、本を読んだりして意図的に読み書きの機会を増やす必要があります。
             </p>
           </div>
         </Pane>
@@ -52,58 +67,67 @@ export const LearnEnglishPage: FC<Props> = () => (
       <Section id='英語学校' className='clear-both'>
         <SectionTitle id='英語学校'>英語学校</SectionTitle>
         <Pane className='space-y-8'>
-          <div>
-            <h3 className='mb-4 text-xl font-bold'>
-              <ExternalLink href='https://www.mikunijapan.org'>
-                みくに国際学園
-              </ExternalLink>
-            </h3>
-            <p>
-              元BYU教授によって設立された英語学校です。
-              新潟県南魚沼郡湯沢町で英語だけで共同生活をしながら英語を学びます。
-              オンラインのコースも用意されています。
-            </p>
-          </div>
+          <p>
+            英語に慣れるための選択肢の1つが英語学校に通うことです。BYU、BYUH、BYUIを目指す人が行くことが多い英語学校を紹介します。
+          </p>
 
-          <div>
-            <h3 className='mb-4 text-xl font-bold'>
-              <ExternalLink href='https://elc.byu.edu/home/program-description-japanese/'>
-                English Language Center(ELC)
-              </ExternalLink>
-            </h3>
-            <p>
-              BYUの運営する英語学校です。
-              BYUのTESOLの大学院生もしくは大学院卒業生が英語を教えてくれます。
-              ELCの生徒はBYUの施設を一部使用できますが、BYUの生徒ではありません。
-            </p>
-          </div>
+          <div className='md:ml-8 space-y-16'>
+            <div>
+              <h3 className='mb-4 text-xl font-bold'>
+                <ExternalLink href='https://www.mikunijapan.org'>
+                  みくに国際学園
+                </ExternalLink>
+              </h3>
+              <p>
+                元BYU教授によって設立された英語学校です。
+                新潟県南魚沼郡湯沢町で英語だけで共同生活をしながら英語を学びます。
+                オンラインのコースも用意されています。
+              </p>
+            </div>
 
-          <div>
-            <h3 className='mb-4 text-xl font-bold'>
-              <ExternalLink href='https://elc.byu.edu/home/program-description-japanese/'>
-                Selnate International School
-              </ExternalLink>
-            </h3>
-            <p>BYUの近くにある英語学校です。 BYUとは提携していません。</p>
-          </div>
+            <div>
+              <h3 className='mb-4 text-xl font-bold'>
+                <ExternalLink href='https://elc.byu.edu/home/program-description-japanese/'>
+                  English Language Center(ELC)
+                </ExternalLink>
+              </h3>
+              <p>
+                BYUが運営する英語学校です。
+                BYUのTESOLの大学院生もしくは大学院卒業生が英語を教えます。
+                ELCの生徒はBYUの施設を一部使用できますが、BYUの生徒ではありません。
+              </p>
+            </div>
 
-          <div>
-            <h3 className='mb-4 text-xl font-bold'>
-              <ExternalLink href='https://www.byuh.edu'>
-                Brigham Young University–Hawaii
-              </ExternalLink>
-            </h3>
-            <p>
-              BYUHは英語学校ではありませんが、英検で入学ができることと、English
-              as an International
-              Language(EIL)という留学生向けの英語のクラスがあることを考えると、英語学校を内包した大学と言えるでしょう。
-              初めのうちははEILクラスを多めに取り、徐々に普通科目を増やしていくという流れになっています。
-              BYUHについて詳しくは
-              <InternalLink href='/byuh' className='text-indigo-500 font-bold'>
-                こちら
-              </InternalLink>
-              。
-            </p>
+            <div>
+              <h3 className='mb-4 text-xl font-bold'>
+                <ExternalLink href='https://elc.byu.edu/home/program-description-japanese/'>
+                  Selnate International School
+                </ExternalLink>
+              </h3>
+              <p>BYUの近くにある英語学校です。 BYUとは提携していません。</p>
+            </div>
+
+            <div>
+              <h3 className='mb-4 text-xl font-bold'>
+                <ExternalLink href='https://www.byuh.edu'>
+                  Brigham Young University–Hawaii
+                </ExternalLink>
+              </h3>
+              <p>
+                BYUHは英語学校ではありませんが、英検で入学ができることと、English
+                as an International
+                Language(EIL)という留学生向けの英語のクラスがあることを考えると、英語学校を内包した大学と言えるでしょう。
+                初めのうちははEILクラスを多めに取り、徐々に普通科目を増やしていくという流れになっています。
+                BYUHについて詳しくは
+                <InternalLink
+                  href='/byuh'
+                  className='text-indigo-500 font-bold'
+                >
+                  こちら
+                </InternalLink>
+                。
+              </p>
+            </div>
           </div>
         </Pane>
       </Section>
