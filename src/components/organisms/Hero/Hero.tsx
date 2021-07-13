8696cd4c6b2b468cfe158ece72acfcb9b155bc82
mizozobu <hamc.m.so.j12@gmail.com>
2021-07-14 01:28:16 +0900

    feat: update index page

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,14 +1,15 @@
-/* eslint-disable @next/next/no-img-element */
 import { useState, FC } from 'react';
 import { Type } from '@c/atoms/Type';
+import { NextImage } from '@c/atoms/NextImage';
 
 export interface Props {}
 
 const texts = [
-  'にどうやって入学するのか?',
-  'で費用はどのくらいかかるのか？',
-  'の違いは何か？',
-  'は自分に合っているのか?',
+  'にはどうやって入学するの?',
+  'で必要な英語力は?',
+  'の費用はどのくらい？',
+  'で奨学金はもらえるの?',
+  'の卒業後の進路は?',
 ];
 
 export const Hero: FC<Props> = () => {
@@ -19,52 +20,60 @@ export const Hero: FC<Props> = () => {
   };
 
   return (
-    <div className='relative bg-white overflow-hidden'>
-      <div className='max-w-7xl mx-auto'>
-        <div className='relative flex justify-center items-center z-10 bg-white lg:max-w-2xl lg:w-full'>
+    <div className='relative'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 grid-rows-1 max-w-7xl mx-auto'>
+        <div className='px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16'>
+          <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
+            <span className='block text-byu'>BYU</span>
+            <span className='block text-byuh'>BYUH</span>
+            <span className='block text-byui'>BYUI</span>
+            <div className='h-32 md:h-48 overflow-hidden'>
+              <Type
+                caretWidth='5px'
+                waitBeforeType={1000}
+                waitAfterType={3000}
+                onBack={handleBack}
+              >
+                {texts[current]}
+              </Type>
+            </div>
+          </h1>
+          <p className='mt-3 md:mt-5 text-base sm:text-lg md:text-xl text-gray-500'>
+            どうやって入学するの？
+            <br />
+            どのくらいの英語力が必要なの？
+            <br />
+            費用はどのくらいかかるの？
+            <br />
+            奨学金はもらえるの？
+            <br />
+            BYU、BYUH、BYUIは何が違うの？
+            <br />
+            卒業後はどんな会社に就職しているの？
+            <br />
+            このような入学前、在学中、卒業後に関する質問に関する情報を在学生と卒業生が情報をまとめました。
+          </p>
+        </div>
+      </div>
+      <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:left-1/2'>
+        <div className='relative w-full h-full'>
           <svg
-            className='hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2'
+            className='hidden lg:block absolute left-0 inset-y-0 h-full w-24 text-white z-10'
             fill='currentColor'
             viewBox='0 0 100 100'
             preserveAspectRatio='none'
             aria-hidden='true'
           >
-            <polygon points='50,0 100,0 50,100 0,100' />
+            <polygon points='0,0 100,0 0,100' />
           </svg>
-
-          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16'>
-            <div>
-              <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl h-80'>
-                <span className='block text-byu'>BYU</span>
-                <span className='block text-byuh'>BYUH</span>
-                <span className='block text-byui'>BYUI</span>
-                <div className='h-48 overflow-hidden'>
-                  <Type
-                    caretWidth='5px'
-                    waitBeforeType={1000}
-                    waitAfterType={3000}
-                    onBack={handleBack}
-                  >
-                    {texts[current]}
-                  </Type>
-                </div>
-              </h1>
-              <p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 h-'>
-                私はBYUに行けるのか？行くメリットあるのか？私にあっているのか？
-                BYUのプロボ、ハワイ、とアイダホのキャンパスに違いがあるのか？
-                上記みたいな質問のお持ちの方のためにBYU卒業生が情報をまとめてみました！
-                入学まで、在学中、と卒業後に関する質問を答えてみましたので、ぜひ拝見ください。
-              </p>
-            </div>
-          </div>
+          <NextImage
+            className='object-cover sm:h-72 md:h-96 lg:w-full lg:h-full'
+            src='/hero.jpeg'
+            alt=''
+            layout='fill'
+            objectFit='cover'
+          />
         </div>
-      </div>
-      <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
-        <img
-          className='h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full'
-          src='https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
-          alt=''
-        />
       </div>
     </div>
   );
