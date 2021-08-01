9170c95ecb6cf6d6611da7fa48b8ed9fcce59392
mizozobu <hamc.m.so.j12@gmail.com>
2021-08-01 14:23:06 +0900

    style: allow header scroll in mobile

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -378,53 +378,36 @@ export const Header: FC<Props> = ({ loading }: Props) => {
             </div>
           </div>
 
-          <Transition
-            show={openMobilePopover}
-            as={Fragment}
-            enter='duration-200 ease-out'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='duration-100 ease-in'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
-          >
-            <Popover.Panel
-              static
-              className='absolute z-20 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden'
+          <Transition show={openMobilePopover}>
+            <Transition.Child
+              as={Fragment}
+              enter='duration-200 ease-out'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='duration-100 ease-in'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
             >
-              <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white'>
-                <div className='pt-5 pb-6 px-5'>
-                  <div className='flex items-center justify-end'>
-                    <div className='-mr-2'>
-                      <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                        <span className='sr-only'>Close menu</span>
-                        <XIcon className='h-6 w-6' aria-hidden='true' />
-                      </Popover.Button>
+              <Popover.Panel
+                static
+                className='absolute z-20 inset-0 p-2 transition transform origin-top-right md:hidden'
+              >
+                <div
+                  className='overflow-y-scroll overscroll-contain rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white'
+                  style={{ maxHeight: 'calc(100vh - 1rem)' }}
+                >
+                  <div className='pt-5 pb-6 px-5'>
+                    <div className='flex items-center justify-end'>
+                      <div className='-mr-2'>
+                        <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                          <span className='sr-only'>Close menu</span>
+                          <XIcon className='h-6 w-6' aria-hidden='true' />
+                        </Popover.Button>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <nav className='grid gap-y-8'>
-                      {schoolLinks.map(({ name, href, icon: Icon }) => (
-                        <Link key={name} href={href}>
-                          <a>
-                            <button
-                              type='button'
-                              className='-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 text-left'
-                              onClick={closeMobilePopOver}
-                            >
-                              <Icon
-                                className='flex-shrink-0 h-6 w-6 text-indigo-700'
-                                aria-hidden='true'
-                              />
-                              <span className='ml-3 text-base font-medium text-gray-900'>
-                                {name}
-                              </span>
-                            </button>
-                          </a>
-                        </Link>
-                      ))}
-                      {additionalSchoolLinks.map(
-                        ({ name, href, icon: Icon }) => (
+                    <div>
+                      <nav className='grid gap-y-8'>
+                        {schoolLinks.map(({ name, href, icon: Icon }) => (
                           <Link key={name} href={href}>
                             <a>
                               <button
@@ -442,71 +425,104 @@ export const Header: FC<Props> = ({ loading }: Props) => {
                               </button>
                             </a>
                           </Link>
-                        ),
-                      )}
-                    </nav>
+                        ))}
+                        {additionalSchoolLinks.map(
+                          ({ name, href, icon: Icon }) => (
+                            <Link key={name} href={href}>
+                              <a>
+                                <button
+                                  type='button'
+                                  className='-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 text-left'
+                                  onClick={closeMobilePopOver}
+                                >
+                                  <Icon
+                                    className='flex-shrink-0 h-6 w-6 text-indigo-700'
+                                    aria-hidden='true'
+                                  />
+                                  <span className='ml-3 text-base font-medium text-gray-900'>
+                                    {name}
+                                  </span>
+                                </button>
+                              </a>
+                            </Link>
+                          ),
+                        )}
+                      </nav>
+                    </div>
+                  </div>
+                  <hr className='px-5' />
+                  <div className='py-6 px-5 space-y-6'>
+                    <div className='grid grid-cols-2 gap-y-4 gap-x-8'>
+                      {applyLinks.map(({ name, href, icon: Icon }) => (
+                        <Link key={name} href={href}>
+                          <a>
+                            <button
+                              type='button'
+                              className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
+                              onClick={closeMobilePopOver}
+                            >
+                              <Icon
+                                className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
+                                aria-hidden='true'
+                              />
+                              {name}
+                            </button>
+                          </a>
+                        </Link>
+                      ))}
+
+                      {careerLinks.map(({ name, href, icon: Icon }) => (
+                        <Link key={name} href={href}>
+                          <a>
+                            <button
+                              type='button'
+                              className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
+                              onClick={closeMobilePopOver}
+                            >
+                              <Icon
+                                className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
+                                aria-hidden='true'
+                              />
+                              {name}
+                            </button>
+                          </a>
+                        </Link>
+                      ))}
+
+                      {otherLinks.map(({ name, href, icon: Icon }) => (
+                        <Link key={name} href={href}>
+                          <a>
+                            <button
+                              type='button'
+                              className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
+                              onClick={closeMobilePopOver}
+                            >
+                              <Icon
+                                className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
+                                aria-hidden='true'
+                              />
+                              {name}
+                            </button>
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
-                <hr className='px-5' />
-                <div className='py-6 px-5 space-y-6'>
-                  <div className='grid grid-cols-2 gap-y-4 gap-x-8'>
-                    {applyLinks.map(({ name, href, icon: Icon }) => (
-                      <Link key={name} href={href}>
-                        <a>
-                          <button
-                            type='button'
-                            className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
-                            onClick={closeMobilePopOver}
-                          >
-                            <Icon
-                              className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
-                              aria-hidden='true'
-                            />
-                            {name}
-                          </button>
-                        </a>
-                      </Link>
-                    ))}
+              </Popover.Panel>
+            </Transition.Child>
 
-                    {careerLinks.map(({ name, href, icon: Icon }) => (
-                      <Link key={name} href={href}>
-                        <a>
-                          <button
-                            type='button'
-                            className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
-                            onClick={closeMobilePopOver}
-                          >
-                            <Icon
-                              className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
-                              aria-hidden='true'
-                            />
-                            {name}
-                          </button>
-                        </a>
-                      </Link>
-                    ))}
-
-                    {otherLinks.map(({ name, href, icon: Icon }) => (
-                      <Link key={name} href={href}>
-                        <a>
-                          <button
-                            type='button'
-                            className='flex items-start text-base font-medium text-gray-900 hover:text-gray-700 text-left'
-                            onClick={closeMobilePopOver}
-                          >
-                            <Icon
-                              className='flex-shrink-0 h-5 w-5 mr-2 my-1 text-indigo-700'
-                              aria-hidden='true'
-                            />
-                            {name}
-                          </button>
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </Popover.Panel>
+            <Transition.Child
+              as={Fragment}
+              enter='duration-200 ease-out'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='duration-100 ease-in'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <Popover.Overlay className='fixed inset-0 bg-black bg-opacity-50' />
+            </Transition.Child>
           </Transition>
         </>
       )}
