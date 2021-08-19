caf0023281ee5e742e4fe411b3f9ec82b09a51d6
mizozobu <hamc.m.so.j12@gmail.com>
2021-08-19 22:33:15 +0900

    revert: faq page(d9ede81)

--- src/lib/CmsClient/CmsClient.constants.ts
+++
@@ -1,3 +0,0 @@
-export const ContentTypeName = {
-  question: 'question',
-} as const;
--- src/lib/CmsClient/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './CmsClient';
-export { default } from './CmsClient';
-export * from './CmsClient.constants';
-export * from './CmsClient.interface';
