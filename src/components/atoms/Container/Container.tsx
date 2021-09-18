81f45c53a97401d2c27e706fe0d93dca27dbf93d
mizozobu <hamc.m.so.j12@gmail.com>
2021-09-18 16:37:07 +0900

    feat: wrap page with article tag

--- src/components/atoms/Container/Container.tsx
+++ src/components/atoms/Container/Container.tsx
@@ -1,9 +1,10 @@
 import { FC, HTMLAttributes } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {}
+export interface Props
+  extends HTMLAttributes<HTMLElementTagNameMap['article']> {}
 
 export const Container: FC<Props> = ({ className = '', ...props }: Props) => (
-  <div
+  <article
     {...props}
     className={classNames(
       'container mx-auto px-8 md:px-16 lg:px-32 my-8 space-y-12 md:space-y-24',
