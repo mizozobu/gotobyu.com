dff8805135417d4de7b026e6dfd65a099086bc27
mizozobu <hamc.m.so.j12@gmail.com>
2021-09-25 01:52:56 +0900

    feat: change container margin

--- src/components/atoms/Container/Container.tsx
+++ src/components/atoms/Container/Container.tsx
@@ -7,7 +7,7 @@ export const Container: FC<Props> = ({ className = '', ...props }: Props) => (
   <article
     {...props}
     className={classNames(
-      'container mx-auto px-8 md:px-16 lg:px-32 my-8 space-y-12 md:space-y-24',
+      'container mx-auto px-8 md:px-16 lg:px-32 mt-12 mb-12 md:mb-36 space-y-12 md:space-y-24',
       className,
     )}
   />
