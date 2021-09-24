e87df78ef35d15a31299cc150d69247b41025875
mizozobu <hamc.m.so.j12@gmail.com>
2021-09-24 23:26:27 +0900

    feat: seo meta for /about

---
+++ src/components/organisms/Seo/Seo.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react';
+import { useRouter } from 'next/router';
+import { NextSeo, NextSeoProps } from 'next-seo';
+import { BASE_URL } from './Seo.constants';
+
+export interface Props extends NextSeoProps {}
+
+export const Seo: FC<Props> = ({ openGraph, ...prop }: Props) => {
+  const router = useRouter();
+
+  return (
+    <NextSeo
+      {...prop}
+      titleTemplate='%s | BYU Management Society Tokyo Chapter'
+      defaultTitle='BYU Management Society Tokyo Chapter'
+      /*
+       * see specification at https://ogp.me
+       * preview at https://www.opengraph.xyz
+       * twitter preview at https://cards-dev.twitter.com/validator
+       */
+      twitter={{
+        cardType: 'summary',
+      }}
+      openGraph={{
+        ...openGraph,
+        url: new URL(router.pathname, BASE_URL).href,
+        locale: 'ja_JP',
+        images: [
+          {
+            url: new URL('/open-graph.png', BASE_URL).href,
+            width: 600,
+            height: 600,
+            alt: 'gotobyu.com',
+          },
+        ],
+      }}
+    />
+  );
+};
+
+/* <Seo
+  title='運営者について'
+  description='このウェブサイトを運営するBYU Management Society Tokyo Chapterについて。'
+  noindex
+  nofollow
+  openGraph={{
+    type: 'article', // "website" for top page
+    title: '運営者について',
+    description:
+      'このウェブサイトを運営するBYU Management Society Tokyo Chapterについて。',
+  }}
+/>; */
