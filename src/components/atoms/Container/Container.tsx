cf6b7ea0978a7adc7b5a69eca440e383d683fb25
mizozobu <hamc.m.so.j12@gmail.com>
2021-11-03 15:09:46 +0900

    feat: smaller padding on sides

--- src/components/atoms/Container/Container.tsx
+++ src/components/atoms/Container/Container.tsx
@@ -8,7 +8,7 @@ export const Container: FC<Props> = ({ className = '', ...props }: Props) => (
   <article
     {...props}
     className={classNames(
-      'max-w-6xl px-8 mx-auto my-12',
+      'max-w-6xl px-4 sm:px-6 mx-auto my-12',
       styles.prose,
       className,
     )}
