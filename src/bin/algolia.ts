182928fc9d067acdefb3efdf9a7f5a3004027ff7
mizozobu <hamc.m.so.j12@gmail.com>
2022-01-30 20:49:35 +0900

    feat: index in postbuild

---
+++ src/bin/algolia.ts
@@ -0,0 +1,32 @@
+import {
+  BYU_PAGE_META,
+  BYUH_PAGE_META,
+  BYUI_PAGE_META,
+  COMPARE_PAGE_META,
+  APPLY_PAGE_META,
+  LEARN_ENGLISH_PAGE_META,
+  CAREER_PAGE_META,
+  INTERNSHIP_PAGE_META,
+  ABOUT_PAGE_META,
+} from '@d';
+import { indexDocument } from '@l/algolia';
+
+/**
+ * index documents in algolia
+ * NOTE: ts-node does not support top level await yet.
+ */
+void (async () => {
+  await Promise.all(
+    [
+      BYU_PAGE_META,
+      BYUH_PAGE_META,
+      BYUI_PAGE_META,
+      COMPARE_PAGE_META,
+      APPLY_PAGE_META,
+      LEARN_ENGLISH_PAGE_META,
+      CAREER_PAGE_META,
+      INTERNSHIP_PAGE_META,
+      ABOUT_PAGE_META,
+    ].map(({ href }) => indexDocument(href)),
+  );
+})();
