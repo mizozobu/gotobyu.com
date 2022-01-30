99b4d14fd0c0df48b2bbf5be6762b38d59ce3aaf
mizozobu <hamc.m.so.j12@gmail.com>
2022-01-30 11:12:44 +0900

    feat: index in algolia in getStaticProps

--- src/lib/algolia/algolia.constants.ts
+++ src/lib/algolia/algolia.constants.ts
@@ -1,4 +1,11 @@
+import { resolve } from 'path';
+
 /**
  * heading tags
  */
 export const HEADING_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
+
+/**
+ * absolute path to pages directory
+ */
+export const PAGE_BASE_PATH = resolve(__dirname, '..');
