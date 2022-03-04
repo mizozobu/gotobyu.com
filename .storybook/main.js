25e2ab7f15a16a9a588be369f4204c38b075a172
mizozobu <hamc.m.so.j12@gmail.com>
2022-03-05 00:08:10 +0900

    feat: use next/router in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const { resolve } = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.css$/,
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import * as NextImage from 'next/image';
-import '../src/styles/globals.css';
 import { RecoilRoot } from 'recoil';
+import '../src/styles/globals.css';
 
 // see https://github.com/vercel/next.js/issues/18393#issuecomment-783269086
 const OriginalNextImage = NextImage.default;
@@ -23,6 +24,9 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 };
 
