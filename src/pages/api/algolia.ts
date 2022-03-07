293595fa8c8146819c7d111f42875ce46b058090
mizozobu <hamc.m.so.j12@gmail.com>
2022-03-07 23:26:48 +0900

    feat: cache serverless function response

--- src/pages/api/algolia.ts
+++ src/pages/api/algolia.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { ValuesType } from 'utility-types';
 // import {
 //   BYU_PAGE_META,
 //   BYUH_PAGE_META,
@@ -12,57 +13,62 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 // } from '@d/links';
 // import { indexDocument } from '@l/algolia/node';
 
-// /** whether documents are indexed */
-// let isIndexed = false;
-// /** time indexed */
-// let indexedAt: Date | undefined;
+/**
+ * Index status
+ */
+const STATUS = {
+  /** Indexed */
+  indexed: 'indexed',
+  /** Skipped */
+  skipped: 'skipped',
+  /** Failed */
+  failed: 'failed',
+} as const;
+/** Time indexed at */
+let indexedAt: Date | undefined;
 
-// /**
-//  * Index documents in algolia.
-//  * Allowed to be called only once.
-//  */
-// export default async (
-//   req: NextApiRequest,
-//   res: NextApiResponse<{
-//     status: 'indexed' | 'skipped' | 'failed';
-//     indexedAt: string;
-//   }>,
-// ) => {
-//   if (isIndexed && indexedAt) {
-//     // Not Modified(no message body)
-//     // see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/304
-//     res.status(304).end();
-//   } else {
-//     isIndexed = true;
-//     indexedAt = new Date();
+/**
+ * Index documents in algolia.
+ * Allowed to be called only once.
+ */
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<{
+    status: ValuesType<typeof STATUS>;
+    indexedAt: Date;
+  }>,
+  // eslint-disable-next-line @typescript-eslint/require-await
+): Promise<void> => {
+  if (indexedAt !== undefined) {
+    res.setHeader('Cache-Control', 's-maxage=86400');
+    res.status(200).json({
+      status: STATUS.skipped,
+      indexedAt,
+    });
+  } else {
+    indexedAt = new Date();
 
-//     try {
-//       await Promise.all(
-//         [
-//           BYU_PAGE_META,
-//           BYUH_PAGE_META,
-//           BYUI_PAGE_META,
-//           COMPARE_PAGE_META,
-//           APPLY_PAGE_META,
-//           LEARN_ENGLISH_PAGE_META,
-//           CAREER_PAGE_META,
-//           INTERNSHIP_PAGE_META,
-//           ABOUT_PAGE_META,
-//         ].map(({ href }) => indexDocument(href)),
-//       );
-//       res
-//         .status(200)
-//         .json({ status: 'indexed', indexedAt: indexedAt.toISOString() });
-//     } catch (err) {
-//       // eslint-disable-next-line no-console
-//       console.error(err);
-//       res
-//         .status(500)
-//         .json({ status: 'failed', indexedAt: new Date().toISOString() });
-//     }
-//   }
-// };
-
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(410).end();
+    try {
+      // await Promise.all(
+      //   [
+      //     BYU_PAGE_META,
+      //     BYUH_PAGE_META,
+      //     BYUI_PAGE_META,
+      //     COMPARE_PAGE_META,
+      //     APPLY_PAGE_META,
+      //     LEARN_ENGLISH_PAGE_META,
+      //     CAREER_PAGE_META,
+      //     INTERNSHIP_PAGE_META,
+      //     ABOUT_PAGE_META,
+      //   ].map(({ href }) => indexDocument(href)),
+      // );
+      res.setHeader('Cache-Control', 's-maxage=86400');
+      res.status(200).json({ status: STATUS.indexed, indexedAt });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      res.setHeader('Cache-Control', 's-maxage=86400');
+      res.status(200).json({ status: STATUS.failed, indexedAt });
+    }
+  }
 };
