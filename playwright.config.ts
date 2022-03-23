dfed6c73741f6fa216be6a060edbffe0c15d0e30
mizozobu <hamc.m.so.j12@gmail.com>
2022-03-24 00:20:15 +0900

    test: init playwright e2e test

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'chromium mobile',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'webkit mobile',
+      use: { ...devices['iPhone 13'] },
+    },
+  ],
+};
+
+export default config;
