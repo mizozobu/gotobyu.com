22954400946c6e7076eddad115b539c3d5378806
mizozobu <hamc.m.so.j12@gmail.com>
2022-04-02 22:56:55 +0900

    test: take screenshots

--- e2e/e2e.util.ts
+++ e2e/e2e.util.ts
@@ -1,8 +1,8 @@
-import { TestInfo } from '@playwright/test';
+import type { TestInfo, Page } from '@playwright/test';
 import { getEnvVar } from '@l/env';
 
 /**
- * resolve a given path to an absolute URL
+ * Resolve a given path to an absolute URL
  * @param path path
  * @returns absolute URL
  */
@@ -19,3 +19,40 @@ export const toSnapshotPath = (name: string, testInfo: TestInfo): string =>
   `.reg/actual/${testInfo.titlePath[0].split('.')[0]}/${name}.${
     testInfo.project.name
   }.png`;
+
+/**
+ * Load lazy-load elements on the page
+ * @param page PlayWright page object
+ */
+export const loadLazyElements = async (
+  page: Page,
+  options: { waitAfterZoomout?: number; waitAfterZoomIn?: number } = {},
+): Promise<void> =>
+  page.evaluate(async ({ waitAfterZoomout = 1000, waitAfterZoomIn = 1000 }) => {
+    /** option1: load by zoom */
+    document.documentElement.style.transform = 'scale(0.01)';
+    document.getElementById('content')?.scrollIntoView();
+    await new Promise((r) => {
+      setTimeout(r, waitAfterZoomout);
+    });
+    document.documentElement.style.transform = 'scale(1)';
+    window.scrollTo(0, 0);
+    await new Promise((r) => {
+      setTimeout(r, waitAfterZoomIn);
+    });
+
+    /** option 2: load by scroll */
+    // const vh = Math.max(
+    //   document.documentElement.clientHeight || 0,
+    //   window.innerHeight || 0,
+    // );
+
+    // let i = 1;
+    // while (vh * i < document.documentElement.scrollHeight) {
+    //   console.log('scroll');
+    //   window.scrollTo({ top: vh * i });
+    //   // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
+    //   await new Promise((r) => setTimeout(r, 500));
+    //   i += 1;
+    // }
+  }, options);
--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,11 +1,15 @@
 import { test } from '@playwright/test';
-import { toAbsUrl, toSnapshotPath } from '@e/e2e.util';
+import { toAbsUrl, toSnapshotPath, loadLazyElements } from '@e/e2e.util';
 
 test('take screenshots', async ({ page }, testInfo) => {
   await page.goto(toAbsUrl('/'));
+  await loadLazyElements(page);
+  await page.waitForSelector('text=にはどうやって入学するの?');
 
   await page.screenshot({
     path: toSnapshotPath('init', testInfo),
     fullPage: true,
+    animations: 'disabled',
+    mask: [page.locator('#animated-circulating-circles')],
   });
 });
