b49fb8951ba946ffcdca6dc1b09edeed46077123
mizozobu <hamc.m.so.j12@gmail.com>
2022-04-02 22:56:55 +0900

    test: take screenshots

--- e2e/404.spec.ts
+++ e2e/404.spec.ts
@@ -1,11 +1,14 @@
 import { test } from '@playwright/test';
-import { toAbsUrl, toSnapshotPath } from '@e/e2e.util';
+import { toAbsUrl, toSnapshotPath, loadLazyElements } from '@e/e2e.util';
 
 test('take screenshots', async ({ page }, testInfo) => {
   await page.goto(toAbsUrl('/404'));
+  await loadLazyElements(page);
 
   await page.screenshot({
     path: toSnapshotPath('init', testInfo),
     fullPage: true,
+    animations: 'disabled',
+    mask: [page.locator('#animated-circulating-circles')],
   });
 });
--- e2e/schools/byu.spec.ts
+++ e2e/schools/byu.spec.ts
@@ -1,11 +1,13 @@
 import { test } from '@playwright/test';
-import { toAbsUrl, toSnapshotPath } from '@e/e2e.util';
+import { toAbsUrl, toSnapshotPath, loadLazyElements } from '@e/e2e.util';
 
 test('take screenshots', async ({ page }, testInfo) => {
   await page.goto(toAbsUrl('/schools/byu'));
+  await loadLazyElements(page);
 
   await page.screenshot({
     path: toSnapshotPath('init', testInfo),
     fullPage: true,
+    animations: 'disabled',
   });
 });
