2a7c9cbb22fdc049955f98f6edeeff49bbecf76c
mizozobu <hamc.m.so.j12@gmail.com>
2022-04-10 02:05:45 +0900

    test: watch console error and encapsulate page

--- e2e/ScreenshotPage.ts
+++ e2e/ScreenshotPage.ts
@@ -1,4 +1,5 @@
 import type { Page } from '@playwright/test';
+import type { PageFunction } from 'playwright-core/types/structs';
 import type { SinonFakeTimers } from 'sinon';
 import {
   toAbsUrl,
@@ -18,7 +19,7 @@ declare global {
  * Page object model to capture screenshots
  */
 export class ScreenshotPage {
-  public readonly page: Page;
+  private readonly page: Page;
 
   /**
    * @constructor
@@ -32,25 +33,59 @@ export class ScreenshotPage {
    * Initialize a give page for screenshot ready
    * @param path relative path
    */
-  async init(path: string): Promise<void> {
+  public async init(path: string): Promise<void> {
+    this.watchConsoleError();
     await Promise.all([injectScripts(this.page), fakeTimers(this.page)]);
     await this.goto(path);
     await loadLazyElements(this.page);
   }
 
   /**
+   * Watch for console errors
+   */
+  private watchConsoleError(): void {
+    this.page.on('console', (message) => {
+      if (message.type() === 'error') {
+        throw new Error(message.text());
+      }
+    });
+  }
+
+  /**
    * Navigate to a given path
    * @param path relative path
    */
-  async goto(path: string): Promise<void> {
+  public async goto(path: string): Promise<void> {
     await this.page.goto(toAbsUrl(path));
+  }
+
+  /**
+   * Returns the current playwright page object
+   * @returns Page
+   */
+  public getPage(): Page {
+    return this.page;
+  }
+
+  /**
+   * Page.evaluate wrapper
+   * @param pageFunction Callback to be executed
+   * @param arg Args passed to the callback
+   * @see {@link Page.evaluate}
+   */
+  public evaluate<R>(
+    pageFunction: PageFunction<void, R>,
+    arg?: unknown,
+  ): Promise<R>;
+  public evaluate<T, U>(pageFunction: PageFunction<U, T>, arg: U): Promise<T> {
+    return this.page.evaluate<T, U>(pageFunction, arg);
   }
 
   /**
    * Take a screenshot
    * @returns Screenshot buffer
    */
-  async screenshot(): Promise<Buffer> {
+  public async screenshot(): Promise<Buffer> {
     // workaround to avoid sticking the sticky header in the middle of a page on a full-size screenshot
     await this.page.evaluate(() => {
       document
--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -3,9 +3,7 @@ import { test, expect } from '@e/e2e.fixture';
 test('visual regression test', async ({ screenshotPage }) => {
   await screenshotPage.init('/');
   // tick the clock ahead to type "にはどうやって入学するの?"
-  await screenshotPage.page.evaluate(async () =>
-    window.__clock.tickAsync(75 * 13),
-  );
+  await screenshotPage.evaluate(async () => window.__clock.tickAsync(75 * 13));
 
   const screenshot = await screenshotPage.screenshot();
 
