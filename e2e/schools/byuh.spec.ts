df32e3ec443aaa72e3b9f9fad951477236fab561
mizozobu <hamc.m.so.j12@gmail.com>
2022-04-11 00:03:50 +0900

    test: add tags to tests

--- e2e/schools/byuh.spec.ts
+++ e2e/schools/byuh.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@e/e2e.fixture';
 
-test('visual regression test', async ({ screenshotPage }) => {
+test('visual regression test @vrt', async ({ screenshotPage }) => {
   await screenshotPage.init('/schools/byuh');
 
   const screenshot = await screenshotPage.screenshot();
