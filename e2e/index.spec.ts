a99870893579822e4795eb2a9299cba08d85713a
mizozobu <hamc.m.so.j12@gmail.com>
2022-04-11 00:03:50 +0900

    test: add tags to tests

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@e/e2e.fixture';
 
-test('visual regression test', async ({ screenshotPage }) => {
+test('visual regression test @vrt', async ({ screenshotPage }) => {
   await screenshotPage.init('/');
   // tick the clock ahead to type "にはどうやって入学するの?"
   await screenshotPage.evaluate(async () => window.__clock.tickAsync(75 * 13));
