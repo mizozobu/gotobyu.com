763192dd85af7cf9e01b1db968d7d081c9ca94be
mizozobu <hamc.m.so.j12@gmail.com>
2022-08-25 23:45:08 +0900

    fix: update alphavantage API

--- src/lib/forex/forex.interface.ts
+++ src/lib/forex/forex.interface.ts
@@ -1,17 +1,23 @@
 /**
- * response from https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE
+ * response from https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=USD&to_symbol=JPY&interval=60min
  */
 export interface AlphaVantageForexResponse {
-  'Realtime Currency Exchange Rate': {
-    '1. From_Currency Code': string;
-    '2. From_Currency Name': string;
-    '3. To_Currency Code': string;
-    '4. To_Currency Name': string;
-    '5. Exchange Rate': string;
-    '6. Last Refreshed': string;
+  'Meta Data': {
+    '1. Information': string;
+    '2. From Symbol': string;
+    '3. To Symbol': string;
+    '4. Last Refreshed': string;
+    '5. Interval': string;
+    '6. Output Size': string;
     '7. Time Zone': string;
-    '8. Bid Price': string;
-    '9. Ask Price': string;
+  };
+  'Time Series FX (60min)': {
+    [key: string]: {
+      '1. open': string;
+      '2. high': string;
+      '3. low': string;
+      '4. close': string;
+    };
   };
 }
 
