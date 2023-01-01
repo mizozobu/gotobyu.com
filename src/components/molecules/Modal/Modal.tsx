c54adc39daffcd8a1ca8974aa5122ddc1059a9d6
mizozobu <hamc.m.so.j12@gmail.com>
2023-01-01 20:10:45 +0900

    refactor: rm dupelicated createPortal for Modal

--- src/components/molecules/Modal/Modal.tsx
+++ src/components/molecules/Modal/Modal.tsx
@@ -1,6 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, type ReactNode } from 'react';
-import { createPortal } from 'react-dom';
 
 /**
  * Props for {@link Modal}
@@ -15,33 +14,31 @@ interface Props {
 }
 
 /**
- * Menu dialog for mobile
+ * Modal
  */
-export const Modal = ({ isOpen, onClose, children }: Props): JSX.Element =>
-  createPortal(
-    <Transition
-      show={isOpen}
-      as={Fragment}
-      enter='duration-200 ease-out'
-      enterFrom='opacity-0 scale-95'
-      enterTo='opacity-100 scale-100'
-      leave='duration-100 ease-in'
-      leaveFrom='opacity-100 scale-100'
-      leaveTo='opacity-0 scale-95'
+export const Modal = ({ isOpen, onClose, children }: Props): JSX.Element => (
+  <Transition
+    show={isOpen}
+    as={Fragment}
+    enter='duration-200 ease-out'
+    enterFrom='opacity-0 scale-95'
+    enterTo='opacity-100 scale-100'
+    leave='duration-100 ease-in'
+    leaveFrom='opacity-100 scale-100'
+    leaveTo='opacity-0 scale-95'
+  >
+    <Dialog
+      className='fixed inset-0 z-20 p-4 md:p-16 lg:p-[12vh]'
+      open={isOpen}
+      onClose={onClose}
     >
-      <Dialog
-        className='fixed inset-0 z-20 p-4 md:p-16 lg:p-[12vh]'
-        open={isOpen}
-        onClose={onClose}
-      >
-        <div
-          className='fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm'
-          onClick={onClose}
-          aria-hidden='true'
-          data-testid='modal-overlay'
-        />
-        {children}
-      </Dialog>
-    </Transition>,
-    document.body,
-  );
+      <div
+        className='fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm'
+        onClick={onClose}
+        aria-hidden='true'
+        data-testid='modal-overlay'
+      />
+      {children}
+    </Dialog>
+  </Transition>
+);
