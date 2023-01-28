f6f8db154c453e70406fba1b6506857cceec9d31
mizozobu <hamc.m.so.j12@gmail.com>
2023-01-28 22:24:08 +0900

    fix: fix icon type

--- src/types/icon.d.ts
+++ src/types/icon.d.ts
@@ -1,4 +1,6 @@
 /**
  * Icon
  */
-export type IconComponentType = (props: { className?: string }) => JSX.Element;
+export type IconComponentType = (props: {
+  className?: string;
+}) => JSX.Element | null;
