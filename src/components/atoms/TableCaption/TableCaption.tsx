7c8c1fe43f5787e0a4776de82e82fd2a0f6c571d
mizozobu <hamc.m.so.j12@gmail.com>
2023-03-29 19:54:09 +0900

    refactor: use tailwind caption util

--- src/components/atoms/TableCaption/TableCaption.tsx
+++ src/components/atoms/TableCaption/TableCaption.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import type { ComponentPropsWithoutRef } from 'react';
-import styles from './TableCaption.module.css';
 
 /**
  * Props for {@link TableCaption}
@@ -19,10 +18,13 @@ export const TableCaption = ({
   children,
   ...props
 }: Props): JSX.Element => (
-  <caption className={classNames('relative py-1', styles['table-caption'])}>
+  <caption className='relative mt-4 caption-bottom whitespace-nowrap py-1 text-left text-xs md:text-right'>
     <span
       {...props}
-      className={classNames(className, 'rounded-full bg-gray-100 px-2 py-1')}
+      className={classNames(
+        className,
+        'mt-4 whitespace-nowrap rounded-full bg-gray-100 px-2 py-1 text-xs',
+      )}
       aria-hidden='true'
       data-noindex='true'
     >
