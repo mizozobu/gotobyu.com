82502bdc44c7552586b763f9491f18b8cc63666b
mizozobu <hamc.m.so.j12@gmail.com>
2024-01-03 14:51:30 +0900

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,md,mdx}'],
   theme: {
     extend: {
@@ -17,3 +18,5 @@ export default {
     },
   },
 };
+
+export default config;
